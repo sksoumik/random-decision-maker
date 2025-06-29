a54b92db65a86a973bd2be54610a86502eaf3b9d
Sadman Kabir Soumik <sadmanks@gmail.com>
2025-06-29 13:32:34 +0600

    Fix build errors and prepare for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index-beautiful.css'
-import App from './App-fixed-hand.tsx'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
